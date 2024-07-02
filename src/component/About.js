cdfaf9ab610b4381703d9b3f12181c504bcce83c
Prakarsh333 <ayush.raghuvanshi@students.iiit.ac.in>
2024-07-02 15:36:03 +0530

    css

--- src/component/About.js
+++ src/component/About.js
@@ -15,7 +15,11 @@ function About() {
         <p>lorem ipsume dkfjdkfj lorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfjlorem ipsume dkfjdkfj</p>
         <br></br>
         <br></br>
-        <NavLink to="/" class="bg-blue-400 m-2 p-2 rounded-lg hover:text-white">Button</NavLink>
+        <NavLink to="/" >
+            <div class="bg-blue-400 w-20 flex justify-center m-2 p-2 rounded-md hover:text-white">
+                Button
+            </div>
+        </NavLink>
     </div>
   )
 }
